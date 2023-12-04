34eec4f227266eed37db6a495dbacb177922bcb9
lazar <lazar.srb.v@gmail.com>
2023-12-04 21:35:42 +0100

    Initial commit1

--- web.py
+++ web.py
@@ -17,7 +17,7 @@ for index, todo in enumerate(todos):
         todos.pop(index)
         functions.write_todos(todos)
         del st.session_state[todo]
-        st.experimental_rerun()
+        st.rerun()
 
 
 st.text_input(label='', placeholder='Add new todo...',
